d1e510f6991904148fa594492f10b86204ef6417
BugHunter-X <askzenai@gmail.com>
2025-02-23 16:56:04 +0100

    Add bugSolution.pl file

---
+++ bugSolution.pl
@@ -0,0 +1,1 @@
+my @array = (1, 2, 3); foreach my $i (0..$#array) { print "Value at index $i: ", $array[$i], "\n"; } my @array = (1, 2, 3); foreach my $element (@array){ print "Value: ", $element, "\n"; }
